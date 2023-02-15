b68dc42daa694fd135b4e95057182881326f25ea
MrBlobbi <azpacker96@gmail.com>
2023-02-15 09:26:41 +0000

    topics buttons

--- components/home/topics/topics.js
+++ components/home/topics/topics.js
@@ -1,10 +1,17 @@
 export default [
-    <button href="https://reactjs.org/" target="_" alt="Link to React documentation" className="bg-sky-500 hover:bg-sky-700">React</button>,
-    "State hook",
-    "Effect hook",
-    "Next.js",
-    "Node.js",
-    "Git",
-    "React components",
-    "Mobile responsive design"
+    <button href="https://reactjs.org/" target="_" alt="Link to documentation" className="bg-sky-500 hover:bg-sky-700 rounded">React</button>,
+    <button href="https://reactjs.org/docs/hooks-state.html" target="_" alt="Link to documentation" className="bg-sky-500 hover:bg-sky-700 rounded">State hook</button>,
+    ,
+    <button href="https://reactjs.org/docs/hooks-effect.html" target="_" alt="Link to documentation" className="bg-sky-500 hover:bg-sky-700 rounded">Effect hook</button>,
+    ,
+    <button href="https://nextjs.org/docs/getting-started" target="_" alt="Link to documentation" className="bg-sky-500 hover:bg-sky-700 rounded">Next.js</button>,
+    ,
+    <button href="https://nodejs.org/en/docs/" target="_" alt="Link to documentation" className="bg-sky-500 hover:bg-sky-700 rounded">Node.js</button>,
+    ,
+    <button href="https://git-scm.com/book/en/v2/Git-Basics-Getting-a-Git-Repository" target="_" alt="Link to documentation" className="bg-sky-500 hover:bg-sky-700 rounded">Git</button>,
+    ,
+    <button href="https://reactjs.org/docs/components-and-props.html" target="_" alt="Link to documentation" className="bg-sky-500 hover:bg-sky-700 rounded">React Components</button>,
+    ,
+    <button href="https://www.w3schools.com/html/html_responsive.asp" target="_" alt="Link to React documentation" className="bg-sky-500 hover:bg-sky-700 rounded">Mobile responsive design</button>,
+
 ];
